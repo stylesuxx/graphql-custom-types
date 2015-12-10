5057b0d22aa2675c10082fb494b39f6b15097a72
stylesuxx <stylesuxx@gmail.com>
2015-12-10 17:32:56 +0100

    added regex factory and adapted accordingly

--- src/scalars.js
+++ src/scalars.js
@@ -38,26 +38,3 @@ export const GraphQLEmail = regexFactory({
   description:'The Email scalar type represents E-Mail addresses compliant to RFC 822.',
   error: 'Query error: Not a valid Email address'
 });
-
-export const GraphQLEmail_ = new GraphQLScalarType({
-  name: 'Email',
-  description:'The Email scalar type represents E-Mail addresses compliant to RFC 822.',
-  serialize: value => {
-    return value;
-  },
-  parseValue: value => {
-    return value;
-  },
-  parseLiteral: ast => {
-    if (ast.kind !== Kind.STRING) {
-      throw new GraphQLError('Query error: Can only parse strings got a: ' + ast.kind, [ast]);
-    }
-
-    var re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-    if(!re.test(ast.value)) {
-      throw new GraphQLError('Query error: Not a valid Email address', [ast]);
-    }
-
-    return ast.value;
-  }
-});
