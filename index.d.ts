0ff7fb784f36c0a430665368289497d2020adda2
Adrey Kun <a.kun@fxpro.com>
2017-06-19 13:00:08 +0300

    typescript type defenition

---
+++ index.d.ts
@@ -0,0 +1,46 @@
+// tslint:disable max-classes-per-file
+import {GraphQLScalarType} from "graphql";
+
+declare namespace graphqlCustomTypes {
+
+  type IASTParser<TResult = any> = (ast: {kind: string, value: string}) => TResult;
+
+  interface IFactoryOptions {
+    name: string;
+    regex: RegExp;
+    description: string;
+    error?: string;
+  }
+
+  class Factory {
+    public getRegexScalar(options: IFactoryOptions): GraphQLCustomScalarType;
+
+    public getCustomScalar(name: string, description: string, parser: IASTParser): GraphQLCustomScalarType;
+  }
+
+  class GraphQLCustomScalarType extends GraphQLScalarType {
+    constructor(name: string, description: string, parser: IASTParser);
+  }
+
+  const GraphQLEmail: GraphQLCustomScalarType;
+
+  const GraphQLURL: GraphQLCustomScalarType;
+
+  const GraphQLDateTime: GraphQLCustomScalarType;
+
+  const GraphQLUUID: GraphQLCustomScalarType;
+
+  class GraphQLPassword extends GraphQLCustomScalarType {
+    constructor(min?: number, max?: number, alphabet?: boolean, complexity?: {
+      alphaNumeric?: boolean,
+      mixedCase?: boolean,
+      specialChars?: boolean,
+    });
+  }
+
+  class GraphQLLimitedString extends GraphQLCustomScalarType {
+    constructor(min?: number, max?: number, alphabet?: boolean)
+  }
+}
+
+export = graphqlCustomTypes;
